7c2c97e5d7251bbe80aa51393ad8214e22be7e70
Iván Izaguirre <ivanizag@gmail.com>
2021-09-17 20:05:20 +0200

    Fix the download URL

--- download.bat
+++ download.bat
@@ -1,4 +1,4 @@
 mkdir sd
-powershell -Command "Invoke-WebRequest https://cdn.hackaday.io/files/1599736844284832/S220718-R240620_IOS-Z80-MBC2.zip -OutFile sd.zip"
+powershell -Command "Invoke-WebRequest https://cdn.hackaday.io/files/1599736844284832/SD-S220718-R240620-v1.zip -OutFile sd.zip"
 powershell -Command "Expand-Archive -DestinationPath sd -LiteralPath sd.zip"
 del sd.zip
